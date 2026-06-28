cd0204f4a0cb49a365889a05a9d7e9606655cfc8
Osama Alhudhaif <osama.alhudhaif@gmail.com>
2026-06-28 18:13:42 +0300

    learn

---
+++ main.py
@@ -0,0 +1,10 @@
+class Home:
+    def __init__(self):
+        self.kitch()
+        self.bedroom()
+    def kitch(self):
+        print("This is the kitchen.")
+    def bedroom(self):
+        print("This is the bedroom.")
+
+x = Home()
